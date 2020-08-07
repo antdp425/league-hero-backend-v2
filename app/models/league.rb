56f8104720dc302004286f726ac1d5b5dbe4cb9a
antdp425 <antdp425@aol.com>
2020-08-06 21:51:20 -0400

    adds team validations for uniqueness of nname, presence of name email phone associated league, and length + numericality of phone

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,3 +1,7 @@
 class League < ApplicationRecord
    has_many :teams, dependent: :destroy
+   validates_uniqueness_of :name, scope: [:league_id], message: "must be unique"
+   validates :name, :league_id, :email, :phone, presence: :true
+   validates :phone, length: {is: 10}, numericality: { only_integer: true , message: "only allows numbers" }
+   
 end
