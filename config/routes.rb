b324c6f65f038147e311e9509e66c0c4c98eb985
antdp425 <antdp425@aol.com>
2020-08-06 21:24:27 -0400

    seeds db

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :teams
-  resources :leagues
+  resources :leagues do 
+    resources :teams
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+   League.create(name: Faker::Sports::Football.competition, league_format: "11v11")
+ end
+ 
+ 5.times do
+    League.first.teams.create(name:Faker::Team.name.titleize,
+        email:Faker::Internet.email,
+         phone:"6564452424")
+    League.second.teams.create(name:Faker::Team.name.titleize,
+        email:Faker::Internet.email,
+         phone:"6564452424")
+    League.third.teams.create(name:Faker::Team.name.titleize,
+        email:Faker::Internet.email,
+         phone:"6564452424")
+    League.fourth.teams.create(name:Faker::Team.name.titleize,
+        email:Faker::Internet.email,
+         phone:"6564452424")
+ end
