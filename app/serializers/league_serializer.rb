629b8dc4fed6ba6c38d687dfda3eccda8b58e36b
antdp425 <antdp425@aol.com>
2020-08-06 22:01:08 -0400

    configures serializers for league and team

--- app/serializers/league_serializer.rb
+++ app/serializers/league_serializer.rb
@@ -1,3 +1,4 @@
 class LeagueSerializer < ActiveModel::Serializer
   attributes :id, :name, :league_format, :start_date, :end_date
+  has_many :teams
 end
--- app/serializers/team_serializer.rb
+++ app/serializers/team_serializer.rb
@@ -1,4 +1,4 @@
 class TeamSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :phone, :paid
-  has_one :league
+  belongs_to :league
 end
