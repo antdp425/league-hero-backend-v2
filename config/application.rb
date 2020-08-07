7ab2d03ff6b672e8bba2c85e99d0433117981676
antdp425 <antdp425@aol.com>
2020-08-06 20:53:49 -0400

    removes active_storage and active_mailbox routes

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,14 @@ module Backend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    initializer(:remove_activestorage_routes, after: :add_routing_paths) {|app|
+      app.routes_reloader.paths.delete_if {|path| path =~ /activestorage/}}
+
+    initializer(:remove_action_mailbox_and_activestorage_routes, after: :add_routing_paths) { |app|
+      app.routes_reloader.paths.delete_if {|path| path =~ /activestorage/}
+      app.routes_reloader.paths.delete_if {|path| path =~ /actionmailbox/ }
+    }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
