70a3a44809637c46c1488553296d36cf24b73e6f
antdp425 <antdp425@aol.com>
2020-08-10 14:30:50 -0400

    adds validations for Team model -> uniqueness of name, presence of all fields, phone length

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,6 @@
 class Team < ApplicationRecord
   belongs_to :league
+  validates_uniqueness_of :name, scope: [:league_id], message: "must be unique"
+  validates :name, :league_id, :email, :phone, presence: :true
+  validates :phone, length: {is: 10}, numericality: { only_integer: true , message: "only allows numbers" }
 end
