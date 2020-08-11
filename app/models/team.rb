2849b5c1f91ce6881909845244ceba505e377c1a
antdp425 <antdp425@aol.com>
2020-08-11 14:01:25 -0400

    updates validation error message when creating a team name if not unique to league

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,6 @@
 class Team < ApplicationRecord
   belongs_to :league
-  validates_uniqueness_of :name, scope: [:league_id], message: "must be unique"
+  validates_uniqueness_of :name, scope: [:league_id], message: "must be unique to league"
   validates :name, :league_id, :email, :phone, presence: :true
   validates :phone, length: {is: 10}, numericality: { only_integer: true , message: "only allows numbers" }
 end
