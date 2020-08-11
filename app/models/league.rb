1fe3cc26254ae6cd99d0365344f6458a557d5587
antdp425 <antdp425@aol.com>
2020-08-10 23:00:56 -0400

    adds validation to League so that end_date cant be before start_date

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,5 +1,13 @@
 class League < ApplicationRecord
    has_many :teams, dependent: :destroy
    validates :name, :league_format, :start_date, :end_date, presence: :true
-   
+   validate :end_date_is_equal_or_after_start_date
+
+   def end_date_is_equal_or_after_start_date
+      return if end_date.blank? || start_date.blank?
+
+      if self.end_date < self.start_date 
+         errors.add(:end_date, "must be after the start date")
+      end
+   end
 end
